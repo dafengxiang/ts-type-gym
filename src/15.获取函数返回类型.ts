1390ef3d5f95eccf4589bb61fe4b7e3008bc6214
王凤祥 <1205679862@qq.com>
2024-09-15 09:02:13 +0800

    feat: 15.获取函数返回类型.ts

---
+++ src/15.获取函数返回类型.ts
@@ -0,0 +1,7 @@
+const fn = (v: boolean) => {
+    if (v) return 1
+    else return 2
+}
+
+type a = MyReturnType<typeof fn> // 应推导出 "1 | 2"
+type MyReturnType<T> = T extends (...ags: any[]) => infer R ? R : never
