dce9b2c9dd0e6a7d40040a1eeffc209ff8325563
王凤祥 <1205679862@qq.com>
2024-09-22 08:18:33 +0800

    feat: 31.Permutation.ts待完成

---
+++ src/31.Permutation(待完成).ts
@@ -0,0 +1,5 @@
+// 用例
+type perm = Permutation<'A' | 'B' | 'C'> // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+
+type Permutation<T extends string> = [T] extends [never] ? [] : T
+// extends any ? [T] : never
